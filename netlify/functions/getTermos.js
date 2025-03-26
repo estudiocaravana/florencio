753f138afe621dae6fe58a8cd79b78bb0237b6c3
edupoch <edupoch@gmail.com>
2025-03-26 19:17:45 +0100

    Prueba de función de Netlify

---
+++ netlify/functions/getTermos.js
@@ -0,0 +1,26 @@
+const fetch = require("node-fetch");
+
+exports.getTermos = async () => {
+  try {
+    const result = await fetch(
+      "https://panel.florenciodelgadogurriaran.gal/items/termo?fields=*.*",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => res.json());
+
+    if (result.errors) {
+      console.log({ errors: result.errors });
+    } else if (!result || !result.data) {
+      console.log({ result });
+      return "No results found.";
+    }
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
