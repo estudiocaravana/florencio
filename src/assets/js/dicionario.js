89f028fd40c79b0199d87fca318a332016dadde3
edupoch <edupoch@gmail.com>
2025-03-26 22:57:37 +0100

    Intentamos login a directus desde la web

---
+++ src/assets/js/dicionario.js
@@ -0,0 +1,21 @@
+import { createDirectus, authentication } from "@directus/sdk";
+
+const directus = createDirectus(
+  "https://panel.florenciodelgadogurriaran.gal"
+).with(authentication());
+
+let loginForm = document.getElementById("loginForm");
+if (loginForm) {
+  loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    try {
+      await directus.login(email, password);
+      alert("Login successful");
+    } catch (error) {
+      alert("Login failed");
+      console.log(error);
+    }
+  });
+}
