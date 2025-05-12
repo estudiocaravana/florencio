94ae5010e5ecbf86ef45e1985091f8f270aff273
edupoch <edupoch@gmail.com>
2025-05-12 22:26:29 +0200

    Creamos un script para obtener las coordenadas de los lugares de la web. Permitimos obtener los datos de la ubicación al tomar los termos

--- netlify/functions/getTermos.js
+++ netlify/functions/getTermos.js
@@ -2,7 +2,7 @@ const { getFromDirectus } = require("../utils/getFromDirectus");
 
 exports.getTermos = async () => {
   return getFromDirectus(
-    "https://panel.florenciodelgadogurriaran.gal/items/termo?fields=*.*&limit=-1",
+    "https://panel.florenciodelgadogurriaran.gal/items/termo?fields=*.*,ubicacion.lugar_id.*&limit=-1",
     "GET"
   );
 };
